f973dbb3004469cfbe4a5193516760c2776e045c
Angelia Horne <AngeliaMHorne@gmail.com>
2023-04-18 15:17:00 -0600

    First page mostly laid out

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React from 'react'
+import { About, Footer, Header, Home, Login, Main, Menu, Orderonline, Reservations} from './containers';
+import { Navbar } from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Homepage
+  <div className="App">
+    <div className="color__bg">
+      <Navbar />
     </div>
-  );
+    <div className="color__bg__box">
+      <Header />
+    </div>
+    <Main />
+    <About />
+    <Footer />
+  </div>
+  )
 }
 
-export default App;
+export default App
