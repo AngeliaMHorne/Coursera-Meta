afcf886fedaaee1f6e81ade854eaeb928f73344b
Angelia Horne <AngeliaMHorne@gmail.com>
2023-04-22 16:34:34 -0600

    additional

--- little-lemon/src/setupTests.js
+++ little-lemon/src/setupTests.js
@@ -1,5 +1,26 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Reservations from '../../containers/reservations/Reservations';
+
+describe('Reservations', () => {
+  it('submits the form with all options selected', async () => {
+    render(<Reservations />);
+
+    const dateInput = screen.getByLabelText('Date:');
+    const timeInput = screen.getByLabelText('Time:');
+    const guestsInput = screen.getByLabelText('Number of guests:');
+    const occasionSelect = screen.getByLabelText('Occasion:');
+
+    const submitButton = screen.getByRole('button', { name: /submit reservation/i });
+
+    fireEvent.change(dateInput, { target: { value: '2023-04-30' } });
+    fireEvent.change(timeInput, { target: { value: '14:30' } });
+    fireEvent.change(guestsInput, { target: { value: '4' } });
+    fireEvent.change(occasionSelect, { target: { value: 'anniversary' } });
+
+    fireEvent.click(submitButton);
+
+    // replace the next line with your own assertion code
+    expect(true).toBe(true);
+  });
+});
